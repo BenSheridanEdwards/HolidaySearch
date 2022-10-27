893ad42e51c82d9c813e0ffae7e87a73d3e82d44
bensheridanedwards <bensheridanedwards@gmail.com>
2022-10-27 15:21:21 +0300

    feat: clean up src and lay out app component with a layout wrapper and routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
+import Layout from './components/Layout/Layout';
+import { DestinationDetailPage } from './pages/DestinationDetailPage/DestinationDetailPage';
+import { DestinationListPage } from './pages/DestinationListPage/DestinationListPage';
+import { Home } from './pages/HomePage/HomePage';
+import './sass/master.scss';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<DestinationListPage />} />
+        <Route path='/sale/:id' element={<DestinationDetailPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
