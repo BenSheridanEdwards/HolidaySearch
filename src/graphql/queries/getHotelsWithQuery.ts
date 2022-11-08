b73d05bda6d16528d15bd275f7cb509c4702f96e
bensheridanedwards <bensheridanedwards@gmail.com>
2022-11-08 14:55:25 +0000

    feat: add pagination to search results.

--- src/graphql/queries/getHotelsWithQuery.ts
+++ src/graphql/queries/getHotelsWithQuery.ts
@@ -1,10 +1,10 @@
 import { gql } from '@apollo/client';
 
 export const GET_HOTELS_WITH_QUERY = gql`
-  query getHotelsWithQuery($query: String!) {
+  query getHotelsWithQuery($query: String!, $offsetNumber: Int!) {
     saleSearch(query: $query, travelTypes: "HOTEL_ONLY") {
       resultCount
-      sales(limit: 10) {
+      sales(limit: 10, offset: $offsetNumber) {
         id
         editorial {
           title
--- src/pages/DestinationListPage/DestinationListPage.tsx
+++ src/pages/DestinationListPage/DestinationListPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { useSearchParams } from 'react-router-dom';
+import { Button } from '../../components/Button/Button';
 import { Card } from '../../components/Card/Card';
 import { PageHeaderWrapper } from '../../components/PageHeaderWrapper/PageHeaderWrapper';
 import type { SaleDestination } from '../../global';
@@ -8,17 +9,28 @@ import { GET_HOTELS_WITH_QUERY } from '../../graphql/queries/getHotelsWithQuery'
 import './DestinationListPage.scss';
 
 export function DestinationListPage() {
+  const [offsetNumber, setOffsetNumber] = useState(0);
+  const [resultCount, setResultCount] = useState(0);
+  const [destinationResults, setDestinationResults] = useState<SaleDestination[]>([]);
+
   const [searchParams] = useSearchParams();
   const urlSearchQuery = searchParams.get('query');
 
-  const { loading, error, data } = useQuery(GET_HOTELS_WITH_QUERY, {
-    variables: { query: urlSearchQuery },
+  const { loading, error } = useQuery(GET_HOTELS_WITH_QUERY, {
+    onCompleted: (data) => {
+      const newDestinationResults = data?.saleSearch?.sales;
+      setDestinationResults([...destinationResults, ...newDestinationResults]);
+      setResultCount(data.saleSearch.resultCount);
+    },
+    variables: { query: urlSearchQuery, offsetNumber: offsetNumber },
   });
 
   if (error) return <div>`Error! ${error.message}`</div>;
-  if (loading) return <div>Loading...</div>;
+  if (loading && !destinationResults) return <div>Loading...</div>;
 
-  const { sales, resultCount } = data.saleSearch;
+  const handleLoadMoreResults = () => {
+    setOffsetNumber(offsetNumber + 10);
+  };
 
   return (
     <div className='DestinationListPage'>
@@ -27,13 +39,13 @@ export function DestinationListPage() {
           Dream <span>destinations</span>, choose the next one.
         </h1>
         <h2 className='DestinationListPage__heading-results-count'>
-          Showing 10 of {resultCount} results
+          Showing {destinationResults.length} of {resultCount} results
         </h2>
       </PageHeaderWrapper>
       <ul className='DestinationListPage__list'>
-        {sales &&
-          sales.length > 0 &&
-          sales.map((destination: SaleDestination) => {
+        {destinationResults &&
+          destinationResults.length > 0 &&
+          destinationResults.map((destination: SaleDestination) => {
             const { title, destinationName } = destination.editorial;
             const [coverImage] = destination.photos;
             const linkUrl = `/sale/${destination.id}`;
@@ -51,6 +63,7 @@ export function DestinationListPage() {
             );
           })}
       </ul>
+      <Button onClick={handleLoadMoreResults}>Load more</Button>
     </div>
   );
 }
